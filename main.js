7fe79f3fb97480f5488191c5e67fbcfa23389e7f
Armando Capriles <armandotechno12@gmail.com>
2023-01-17 21:06:33 -0400

    Carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburger = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburger.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive')
+  desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-  mobileMenu.classList.toggle('inactive')
+  const isAsideClosed = aside.classList.contains('inactive')
+
+  if ( !isAsideClosed ) {
+    aside.classList.add('inactive')
+  }
+
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+  
+  if ( !isMobileMenuClosed ) {
+    mobileMenu.classList.add('inactive')
+  }
+  
+  if ( !isDesktopMenuClosed ) {
+    desktopMenu.classList.add('inactive')
+  }
+
+  aside.classList.toggle('inactive')
 }
