655724c2e6069ea3b5319818f636eb941fbfc638
Armando Capriles <armandotechno12@gmail.com>
2023-01-19 11:08:12 -0400

    Final code

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburger = document.querySelector('.menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
+const productDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburger.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoshoppingCartContainer);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
@@ -21,6 +24,8 @@ function toggleMobileMenu() {
     shoppingCartContainer.classList.add('inactive')
   }
 
+  closeProductDetailAside();
+
   mobileMenu.classList.toggle('inactive');
 }
 
@@ -31,6 +36,12 @@ function toggleCarritoshoppingCartContainer() {
   if ( !isMobileMenuClosed ) {
     mobileMenu.classList.add('inactive')
   }
+
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
+  if ( !isProductDetailClosed ) {
+    productDetailContainer.classList.add('inactive')
+  }
+
   
   if ( !isDesktopMenuClosed ) {
     desktopMenu.classList.add('inactive')
@@ -39,22 +50,36 @@ function toggleCarritoshoppingCartContainer() {
   shoppingCartContainer.classList.toggle('inactive')
 }
 
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive')
+}
+
 const productList = [];
 productList.push({
   name: 'bike',
   price: 120,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
-  name: 'Pantalla',
-  price: 320,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
-productList.push({
-  name: 'Teléfono',
-  price: 190,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-})
+});
+productList.push ({
+  name:'Tennis Montain Bike',
+  price: 2200,
+  image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+  name:'Sunglasses',
+  price: 800,
+  image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+  name:'Bicycle seat bag',
+  price: 876,
+  image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+}); 
 
 function renderProducts(arr) {
   for (product of arr) {
@@ -62,8 +87,9 @@ function renderProducts(arr) {
     productCard.classList.add('product-card')
   
     // product = { name, image, price } -> product.image
-    const productImg = document.createElement('img')
-    productImg.setAttribute('src', product.image)
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside);
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
